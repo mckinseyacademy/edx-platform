a9e246b8357dac14b0817af22bf2002a7861998a
Moeez Zahid <moeezzahid1996@gmail.com>
2020-12-31 11:34:21 +0500

    MCKIN-24885 Fix CourseCreator app not being detected by LMS #2053 conflicts resolved

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -1356,7 +1356,8 @@ INSTALLED_APPS = [
     'contentstore.apps.ContentstoreConfig',
 
     'openedx.core.djangoapps.contentserver',
-    'course_creators',
+    'cms.djangoapps.course_creators',
+    'openedx.core.djangoapps.external_auth',
     'student.apps.StudentConfig',  # misleading name due to sharing with lms
     'openedx.core.djangoapps.course_groups',  # not used in cms (yet), but tests run
     'xblock_config.apps.XBlockConfig',
